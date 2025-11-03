39d1248cc7627da130420900d1c19cb78d0c3e15
KAJAL KUMARI <cutekajal2065@gmail.com>
2025-11-03 23:47:50 +0530

    Add files via upload

---
+++ mega project 1 - jarvis/musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "march":  "https://www.youtube.com/watch?v=vWtyjfxr2Bw",
+    "skyfall":  "https://www.youtube.com/watch?v=HCWvgoTfUjg"
+}
